e159f9960b9b98fb0b5e9bc319954da96f7d4079
Becky Zorn <bczorn@gmail.com>
2020-01-27 17:00:11 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,7 +11,7 @@ class Backer
   def back_project(project)
     @backed_projects << project
     
-    project.backers = self
+    project.backers = 
     
     #x = Project.new(project)
     #x.backers << self
